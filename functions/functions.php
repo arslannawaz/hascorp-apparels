82978badb3c44d1f921e94a91a02cab3149a51c8
arslannawaz <arslnwaz@gmail.com>
2021-07-10 22:55:03 +0500

    added products to all-product page

--- functions/functions.php
+++ functions/functions.php
@@ -1,38 +1,5 @@
 <?php
 
-function viewDetail(){
-    include('includes/database.php');
-    $data=[];
-    $sql = "SELECT * from entry";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $data[]=$row;
-        }
-    } 
-    return $data;
-}
-
-function searchDetail($start_date,$end_date){
-    include('includes/database.php');
-    $sql = "SELECT * from entry where date between '$start_date' and '$end_date'";
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            $data[]=$row;
-        }
-    } 
-    return $data;
-}
-
-function getDetailById($id){
-    include('includes/database.php');
-    $sql = "SELECT * from entry where id='$id'";
-    $result = $conn->query($sql);
-    $row = mysqli_fetch_assoc($result);
-    return $row;
-}
-
 function getSubCategories(){
     include('includes/database.php');
     $data=[];
@@ -50,6 +17,20 @@ function getProducts(){
     include('includes/database.php');
     $data=[];
     $sql = "SELECT * from product";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $data[]=$row;
+        }
+    } 
+    shuffle($data);
+    return $data;
+}
+
+function getProductsBySubCategory($id){
+    include('includes/database.php');
+    $data=[];
+    $sql = "SELECT * from product where sub_category_id = '$id'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
